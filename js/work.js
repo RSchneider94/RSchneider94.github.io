82d73228a22c0943ddd086605ba2c614276650ee
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-09-10 16:52:43 +0200

    Clear example (fakes) portofolio

--- js/work.js
+++ js/work.js
@@ -1,18 +1,18 @@
 let myWorkList = [
-  {
-    title: 'First Project',
-    thumb: 'https://placeimg.com/200/200/tech'
-  },
-  {
-    title: 'Second Project',
-    thumb: 'https://placeimg.com/200/200/tech'
-  },
-  {
-    title: 'Third Project',
-    thumb: 'https://placeimg.com/200/200/tech'
-  },
-  {
-    title: 'Fourth Project',
-    thumb: 'https://placeimg.com/200/200/tech'
-  },
+  // {
+  //   title: 'First Project',
+  //   thumb: 'https://placeimg.com/200/200/tech'
+  // },
+  // {
+  //   title: 'Second Project',
+  //   thumb: 'https://placeimg.com/200/200/tech'
+  // },
+  // {
+  //   title: 'Third Project',
+  //   thumb: 'https://placeimg.com/200/200/tech'
+  // },
+  // {
+  //   title: 'Fourth Project',
+  //   thumb: 'https://placeimg.com/200/200/tech'
+  // },
 ];
