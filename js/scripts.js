a3afddb34255218ea57d74032c9373db641f06b6
Rodrigo Schneider <rodrigoschneider94@gmail.com>
2018-09-10 16:52:08 +0200

    Clear unitilisable code and change contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-  // Tooltip
-  $(function () {
-    $('#item1').tooltip();
-  });
-
   // Smooth Scroll
   var $root = $('html, body');
   $('.navbar a').click(function () {
@@ -19,15 +14,19 @@ $(document).ready(function() {
   });
 
   // Work Section
-  for (let index = 0; index < myWorkList.length; index++) {
-    $('#portfolio-thumbs').append(
-      '<div class="col-6 col-md-3">\
-        <a class="portfolio-caption-container" href="#">\
-          <img class="img-fluid" src="' + myWorkList[index].thumb +'">\
-          <p class="portfolio-caption-title text-center">' + myWorkList[index].title + '</p>\
-        </a>\
-      </div>'
-    );
+  if(myWorkList.length >= 0) {
+    for (let index = 0; index < myWorkList.length; index++) {
+      $('#portfolio-thumbs').append(
+        '<div class="col-6 col-md-3">\
+          <a class="portfolio-caption-container" href="#">\
+            <img class="img-fluid" src="' + myWorkList[index].thumb +'">\
+            <p class="portfolio-caption-title text-center">' + myWorkList[index].title + '</p>\
+          </a>\
+        </div>'
+      );
+    }
+  } else {
+    $('#portfolio-thumbs').append('<h3>Available Soon</h3>');
   }
 
   // Caption On Hover
@@ -37,28 +36,4 @@ $(document).ready(function() {
     $('.portfolio-caption-title', this).hide();
   });
 
-  // Future Tests
-  /* $(window).resize(function () { 
-    var screenHeight = $(window).height();
-    $("#home").css("height", screenHeight);
-  });
-  var screenHeight = $(window).height();
-  $("#home").css("height", screenHeight); */
 });
-
-// Google Maps API
-function initMap() {
-  // Berlin LatLng
-  var myLatLng = {lat: 52.5065133, lng: 13.1445545};
-  var map = new google.maps.Map(document.getElementById('google-maps'), {
-    center: myLatLng,
-    zoom: 10
-  });
-
-  var myFirstMarker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    label: 'RS',
-    title: 'Hello, Career Foundry!'
-  });
-}
